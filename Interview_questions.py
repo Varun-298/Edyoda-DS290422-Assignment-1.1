af291523c4a775c378e474fe850c55593b5378ac
Varun Rohit Vadapalli <105191986+Varun-298@users.noreply.github.com>
2022-06-23 13:57:29 +0530

    Add files via upload

---
+++ Interview_questions.py
@@ -0,0 +1,70 @@
+# 1) How we can convert the given list into the set ?
+# Ans: set(list)
+
+# 2) If we try to access the item outside the list index, then what type of error it may give ?
+# Ans: List index out of range
+
+# 3) In which data type, indexing is not valid ?
+# Ans: Dictionary
+
+# 4) In which data type, duplicate items are not allowed ?
+# Ans: Set
+
+# 5) x=3.123, then int(x) will give
+# Ans: 3
+
+# 6) What will be the output of the following:
+# a = (1,2,3)
+# b = (1,2,3)
+# print(a is b)
+#Ans : True
+
+# 7) What will be output of the following program ? print(3%4)
+# Ans: 3
+
+# 8) What is the use of the ** operator ?
+# Ans: Power
+
+# 9) What is the use of the % operator ?
+# Ans: To find the remainder
+
+# 10) print(1000==300)
+# Ans: False
+
+# 11) What will be output for 100/25 expression?
+# Ans: 4.0
+
+# 12) And, Or and Not all are examples of?
+# Ans: logical Operators
+
+# 13) Which operator can be used to compare two values?
+# Ans: ==
+
+# 14) What will be the output of this statement:
+# print("p" in "Python")
+# Ans: False
+
+# 15) What is the output of the following code:
+# print(9//2)
+# 4
+
+# 16)What is the result of this statement?
+# print(ord('foo'))
+# Ans: It raises an exception
+
+# 17) What is the output of the print() function call? 
+# s = 'foo'
+#t = 'bar'
+# print('barf' in 2 * (s + t))
+# Ans: True
+
+# 18)What will be the output of this expression:
+#'p' + 'q' if '12'.isdigit() else 'r' + 's'
+# Ans: pq
+
+# 19) What will be the output of the following Python code?
+# print('hello'=="hello")
+# Ans: True
+
+# 20) What will be the output of the following Python statement? print(chr(ord('b')+1)) ?
+# Ans: c
